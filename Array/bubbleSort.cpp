74d0ddd745c89ada76d0467f02e2a64c4530fd30
VBsaini <vbsaini2004@gmail.com>
2024-09-22 23:47:42 +0530

    bubble sort

---
+++ Array/bubbleSort.cpp
@@ -0,0 +1,50 @@
+// Bubble Sort Algorithm
+// Problem Statement: Given an array of N integers, write a program to implement the Bubble Sorting algorithm
+
+// Example 1:
+// Input: N = 6, array[] = {13,46,24,52,20,9}
+// Output: 9,13,20,24,46,52
+// Explanation: After sorting we get 9,13,20,24,46,52
+
+
+// Input: N = 5, array[] = {5,4,3,2,1}
+// Output: 1,2,3,4,5
+// Explanation: After sorting we get 1,2,3,4,5
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    cout << "After Using bubble sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    return 0;
+
+}
