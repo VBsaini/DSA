5100583628b0bd579fa2fd81785f91c51f13f33f
VBsaini <vbsaini2004@gmail.com>
2024-10-22 20:16:49 +0530

    Capacity to Ship Packages within D Days

---
+++ BinarySearch/CapacitytoShipPackageswithinDDays.cpp
@@ -0,0 +1,51 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDays(vector<int> &weights, int cap) {
+    int days = 1; //First day.
+    int load = 0;
+    int n = weights.size(); //size of array.
+    for (int i = 0; i < n; i++) {
+        if (load + weights[i] > cap) {
+            days += 1; //move to next day
+            load = weights[i]; //load the weight.
+        }
+        else {
+            //load the weight on the same day.
+            load += weights[i];
+        }
+    }
+    return days;
+}
+
+int leastWeightCapacity(vector<int> &weights, int d) {
+    //Find the maximum and the summation:
+    int low = *max_element(weights.begin(), weights.end());
+    int high = accumulate(weights.begin(), weights.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int numberOfDays = findDays(weights, mid);
+        if (numberOfDays <= d) {
+            //eliminate right half
+            high = mid - 1;
+        }
+        else {
+            //eliminate left half
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> weights = {5, 4, 5, 2, 3, 4, 5, 6};
+    int d = 5;
+    int ans = leastWeightCapacity(weights, d);
+    cout << "The minimum capacity should be: " << ans << "\n";
+    return 0;
+}
+
