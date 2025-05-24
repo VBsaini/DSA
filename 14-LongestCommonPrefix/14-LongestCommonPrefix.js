df99bbfd97b3c1a93a70684c094cd17c67902a1e
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-05-24 23:55:26 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.js
@@ -0,0 +1,18 @@
+// Last updated: 5/24/2025, 11:55:26 PM
+var longestCommonPrefix = function(strs) {
+    let pref = strs[0];
+    let prefLen = pref.length;
+
+    for (let i = 1; i < strs.length; i++) {
+        let s = strs[i];
+        while (pref !== s.substring(0, prefLen)) {
+            prefLen--;
+            if (prefLen === 0) {
+                return "";
+            }
+            pref = pref.substring(0, prefLen);
+        }
+    }
+
+    return pref;    
+};
