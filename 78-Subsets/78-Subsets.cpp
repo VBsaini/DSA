ca49db879375a36d941f16a8d93200ed0710463d
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-05-26 00:57:18 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/26/2025, 12:57:17 AM
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int loop = 1<<nums.size();
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i = 0; i < loop; i++){
+            vector<int> list = {};
+            for(int j = 0; j<n;j++){
+                if(i & (1<<j)){
+                    list.push_back(nums[j]);
+                }
+            }
+            ans.push_back(list);
+        }
+        return ans;
+    }
+};
