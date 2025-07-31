e14862d430fded3c3a0d3bce6884810d3ed0bc1a
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-07-31 17:24:52 +0530

    Time: 14 ms (75.52%), Space: 47.3 MB (42.61%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -6,21 +6,23 @@ public:
         int j = 0;
         vector<int> prev_less(A.size(), -1);
         vector<int> next_less(A.size(), -1);
-        for(int i = 0; i < A.size(); i++) prev_less[i] =  i + 1;
-        for(int i = 0; i < A.size(); i++) next_less[i] = A.size() - i;
-        while(j < A.size()){
-            while(!S.empty() && A[S.top()] > A[j]) {
-                auto x = S.top();
-                next_less[x] = j - x;
-                S.pop();
-            }
-            prev_less[j] = S.empty() ? j+1 :j - S.top();
-            S.push(j);
-            j++;
+        stack<int> Nse;
+        for(int i = A.size() - 1; i >= 0; i--){
+            while(!Nse.empty() && A[Nse.top()] >= A[i]) Nse.pop();
+            next_less[i]  = Nse.empty() ? A.size() : Nse.top();
+            Nse.push(i);
+        }
+        stack<int> st;
+        for(int i = 0; i < A.size(); i++){
+            while(!st.empty() && A[st.top()] > A[i]) st.pop();
+            prev_less[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
     int ans = 0, mod = 1e9 +7;
     for(int i = 0; i < A.size(); i++){
-      ans = (ans + 1LL * A[i]*prev_less[i]*next_less[i])%mod;
+        int left = i - prev_less[i];
+        int right = next_less[i] - i;
+      ans = (ans + 1LL * A[i]*left*right)%mod;
     }
     return ans;
     }
