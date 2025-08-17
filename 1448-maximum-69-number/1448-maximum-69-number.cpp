231849a52d1c043380f3cf45a9861ab0bca28c4d
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-08-17 19:09:15 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.28%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-    //    if(((num + 1) % 10) == 0){
-    //     return num;
-    //    }
        int temp = num;
        int n = -1;
        int i = 0;
        while(temp != 0){
         int val = temp%10;
-        cout << val << " ";
         if(val == 6){
             n = i;
         }
