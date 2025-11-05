d6bd23337813f9a72e72b036ffcb319f9dca9f19
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2025-11-06 00:07:13 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.27%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        traverse(&ans, root);
+        return ans;
+    }
+    void traverse(vector<int>* ans, TreeNode* node){
+        if(node == NULL){
+            return;
+        }
+        cout << node->val << " ";
+        ans->push_back(node->val);
+        traverse(ans, node->left);
+        traverse(ans, node->right);
+
+    }
+};
