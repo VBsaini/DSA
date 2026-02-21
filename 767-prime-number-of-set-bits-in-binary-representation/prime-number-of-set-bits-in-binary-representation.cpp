774b1991fee58faf048c5ffd17562673e9b70e87
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2026-02-21 18:16:43 +0530

    Time: 4 ms (55.49%) | Memory: 7.9 MB (61.81%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> Prime = {2, 3, 5, 7, 11, 13, 17, 19};
         int ans = 0;
         for(int i = left; i <= right; i++){
-            if(find(Prime.begin(), Prime.end(), countBits(i)) != Prime.end()){
+            if(find(Prime.begin(), Prime.end(), __builtin_popcount(i)) != Prime.end()){
                 ans++;
             }
         }
