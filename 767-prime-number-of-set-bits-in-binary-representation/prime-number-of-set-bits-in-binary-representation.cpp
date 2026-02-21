a2e05f0046cc702d546e06a82801a1fe9a95a045
Vaibhav Saini <66212759+VBsaini@users.noreply.github.com>
2026-02-21 18:11:37 +0530

    Time: 27 ms (23.88%) | Memory: 8.1 MB (35.12%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        vector<int> Prime = {2, 3, 5, 7, 11, 13, 17, 19};
+        int ans = 0;
+        for(int i = left; i <= right; i++){
+            if(find(Prime.begin(), Prime.end(), countBits(i)) != Prime.end()){
+                ans++;
+            }
+        }
+        return ans;
+    }
+    int countBits(int number){
+        int bits = 0;
+        while(number != 0){
+            int rem = number%2;
+            if(rem == 1){
+                bits++;
+            }
+            number /= 2;
+        }
+        return bits;
+    }
+};
